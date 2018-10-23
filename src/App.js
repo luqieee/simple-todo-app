b3fa00be21daa97aa6049002088f415fe8bae418
Łukasz Derek <lukasz.derek@gmail.com>
2018-10-23 17:17:41 +0200

    adding tasks and showing them

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import TodoList from './Todo/TodoList'
+import TodoItems from "./Todo/TodoItems";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TodoList/>
-      </div>
-    );
-  }
+
+    inputElement = React.createRef()
+
+    constructor() {
+        super();
+        this.state = {
+            items: [],
+            currentItem: {text: '', key: ''}
+        }
+    };
+
+    handleInput = i => {
+        const itemText = i.target.value;
+        const currentItem = {text: itemText, key: Date.now()};
+        this.setState({
+            currentItem
+        })
+    };
+
+    addItem = e => {
+        e.preventDefault();
+        const newItem = this.state.currentItem;
+        if (newItem.text !== '') {
+            console.log(newItem);
+            const items = [...this.state.items, newItem];
+            this.setState({
+                items: items,
+                currentItem: {text: '', key: ''}
+            })
+        }
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <TodoList
+                    addItem={this.addItem}
+                    handleInput={this.handleInput}
+                    inputElement={this.inputElement}
+                    currentItem={this.state.currentItem}
+                />
+                <TodoItems
+                    entries={this.state.items}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+
+    componentDidUpdate() {
+        this.props.inputElement.current.focus()
+    }
+
     render() {
         return(
             <div className="todoList">
                 <div className="todoListHeader">
-                    <form>
-                        <input placeholder="Zadanie" />
+                    <form onSubmit={this.props.addItem}>
+                        <input
+                            placeholder="Zadanie"
+                            ref={this.props.inputElement}
+                            value={this.props.currentItem.text}
+                            onChange={this.props.handleInput}
+                        />
                         <button type="submit"> Dodaj </button>
                     </form>
                 </div>
